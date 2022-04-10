8d37d39c44c2ee689a1e281b384688e5ff2a3d40
obvMath <ethan@embin.net>
2022-04-10 07:48:36 -0400

    if statement overhaul + more

--- createShortLink.py
+++ createShortLink.py
@@ -8,28 +8,36 @@ import time
 
 url = input('URL to redirect to: ')
 print(' ')
-backhalf = input('Backhalf (inputing "random" will create a 4 character random backhalf consisting of uppercase letters, lowercase letters, and digits.): ')
+customBackhalf = input('Backhalf (inputing "random" will create a 4 character random backhalf consisting of uppercase letters, lowercase letters, and digits.): ')
 
-if backhalf == "random":
-    randomBackhalf = ( ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for i in range(4)) )
-    os.mkdir(randomBackhalf)
-    file = open(f"{randomBackhalf}/index.html","w+")
-    file.write(f'<meta http-equiv="refresh" content="0; URL={url}">')
-    file.close()
-    print(' ')
-    print(f'A short link created at "https://x.obvMath.com/{randomBackhalf}". The short link has been copied to the clipboard.')
-    pyperclip.copy(f"https://x.obvMath.com/{randomBackhalf}")
+if customBackhalf == "random":
+    backhalf = ( ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for i in range(4)) )
+
 else:
-    os.mkdir(backhalf)
-    file = open(f"{backhalf}/index.html","w+")
-    file.write(f'<meta http-equiv="refresh" content="0; URL={url}">')
-    file.close()
-    print(' ')
-    print(f'A short link created at "https://x.obvMath.com/{backhalf}". The short link has been copied to the clipboard.')
-    pyperclip.copy(f"https://x.obvMath.com/{backhalf}")
+    backhalf = customBackhalf
 
+os.mkdir(backhalf)
+file = open(f"{backhalf}/index.html","w+")
+file.write(f'<meta http-equiv="refresh" content="0; URL={url}">')
+file.close()
+print(' ')
+print(f'A short link created at "https://x.obvMath.com/{backhalf}". The short link has been copied to the clipboard.')
+pyperclip.copy(f"https://x.obvMath.com/{backhalf}")
+
+
+os.startfile("..\GitHubDesktop.exe.lnk") # Open GitHub Desktop to remind me to push origin to GitHub Pages so the website actually updates.
 print(' ')
 print('Closing in:')
+print('10')
+time.sleep(1)
+print('9')
+time.sleep(1)
+print('8')
+time.sleep(1)
+print('7')
+time.sleep(1)
+print('6')
+time.sleep(1)
 print('5')
 time.sleep(1)
 print('4')
@@ -41,3 +49,4 @@ time.sleep(1)
 print('1')
 time.sleep(1)
 print('Closing...')
+time.sleep(1)
