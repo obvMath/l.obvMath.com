28b2ab346711e6651ca3a0349ac19802c6c5041c
obvMath <ethan@embin.net>
2022-04-11 15:48:03 -0400

    Update createShortLink.py

--- createShortLink.py
+++ createShortLink.py
@@ -6,10 +6,18 @@ import os
 import pyperclip
 import time
 
-url = input('URL to redirect to: ')
+# Checks if URL entered is valid (starts with http:// or https://)
+isUrlValid = False
+while isUrlValid == False:
+    url = input('URL to redirect to: ')
+    if url.startswith('https://') or url.startswith('http://'):
+        isUrlValid = True
+    else:
+        isUrlValid = False
+        print(' ')
 
+# Checks if short link already exists
 isdir = True
-
 while isdir == True:
     print(' ')
     customBackhalf = input('Backhalf (inputing "random" will create a 4 character random backhalf consisting of uppercase letters, lowercase letters, and digits.): ')
@@ -21,12 +29,12 @@ while isdir == True:
         backhalf = customBackhalf
     isdir = os.path.isdir(backhalf)
 
-os.mkdir(backhalf)
-file = open(f"{backhalf}/index.html","w+")
-file.write(f'<meta http-equiv="refresh" content="0; URL={url}">')
-file.close()
-print(f'A short link created at "https://x.obvMath.com/{backhalf}". The short link has been copied to the clipboard.')
-pyperclip.copy(f"https://x.obvMath.com/{backhalf}")
+os.mkdir(backhalf) # Creates folder
+file = open(f"{backhalf}/index.html","w+") # Created HTML file in folder
+file.write(f'<meta http-equiv="refresh" content="0; URL={url}">') # Writes redirect code to HTML file
+file.close() # Saves the file
+print(f'A short link created at "https://x.obvMath.com/{backhalf}". The short link has been copied to the clipboard.') # Confirmation that short link was created
+pyperclip.copy(f"https://x.obvMath.com/{backhalf}") # Copies created short link to clipboard
 
 os.startfile("..\GitHubDesktop.exe.lnk") # Opens GitHub Desktop to remind me to push origin to GitHub Pages so the website actually updates.
 print(' ')
